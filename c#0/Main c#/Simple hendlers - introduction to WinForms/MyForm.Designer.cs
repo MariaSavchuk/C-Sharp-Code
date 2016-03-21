c80baf63395851c3ccfd5f517612c24171dd9b5e
MariaSavchuk <maccoriacco@gmail.com>
2016-03-21 20:19:07 +0300

    Filenames
    Russian filenames where changed

---
+++ c#0/Main c#/Simple hendlers - introduction to WinForms/MyForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Дурак
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.controlTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(230, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы знаете, как нажать на Нет?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // YesButton
+            // 
+            this.YesButton.Location = new System.Drawing.Point(171, 164);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(91, 56);
+            this.YesButton.TabIndex = 1;
+            this.YesButton.Text = "Да";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
+            this.NoButton.Location = new System.Drawing.Point(570, 164);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(91, 56);
+            this.NoButton.TabIndex = 2;
+            this.NoButton.Text = "Нет";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NoButton_MouseMove);
+            this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
+            this.NoButton.Enter += new System.EventHandler(this.NoButton_Enter);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(298, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 62);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Visible = false;
+            // 
+            // controlTextBox
+            // 
+            this.controlTextBox.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.controlTextBox.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.controlTextBox.Location = new System.Drawing.Point(192, 486);
+            this.controlTextBox.Name = "controlTextBox";
+            this.controlTextBox.Size = new System.Drawing.Size(470, 20);
+            this.controlTextBox.TabIndex = 4;
+            this.controlTextBox.Visible = false;
+            this.controlTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cntrolTextBox_KeyDown);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(842, 509);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.controlTextBox);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "MyForm";
+            this.Text = "Нажмите alt, а потом введите \"Hет\" +Enter, После чего кнопка перестанет убегать.";
+            this.TopMost = true;
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MyForm_MouseMove);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.TextBox controlTextBox;
+    }
+}
+
---
+++ c#0/Main c#/Simple hendlers - introduction to WinForms/MyForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace Дурак
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+        int  par=5,k=1,b=1;
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label2.Text = "Понятно:)";
+            YesButton.Visible = false;
+            NoButton.Visible = false;
+            label1.Visible = false;
+            controlTextBox.Visible = false;
+        }
+        private void MyForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Alt)&&(k!=0))
+            {
+                controlTextBox.Visible = true;
+                controlTextBox.Focus();
+            }
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            label2.Text = "Молодец!";
+            YesButton.Visible = false;
+            NoButton.Visible = false;
+            controlTextBox.Visible = false;
+            label1.Visible = false;
+        }
+
+        private void cntrolTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (controlTextBox.Text.Equals("Нет"))
+                {
+                    k = 0;
+                    b = 0;
+                    e.SuppressKeyPress = true;
+                    controlTextBox.Visible = false;
+                }
+            }
+        }
+
+        private void MyForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (NoButton.Location.X - par <= 0)
+            {
+                NoButton.Location = new Point(NoButton.Location.X + par+NoButton.Width, NoButton.Location.Y);
+            }
+            if (NoButton.Location.X + par >= this.Width - NoButton.Width)
+            {
+                NoButton.Location = new Point(NoButton.Location.X - par-NoButton.Width, NoButton.Location.Y);
+            }
+            if (NoButton.Location.Y - par <= 0)
+            {
+                NoButton.Location = new Point(NoButton.Location.X, NoButton.Location.Y + par+NoButton.Height);
+            }
+            if (NoButton.Location.Y + par >= this.Height -  NoButton.Height-15)
+            {
+                NoButton.Location = new Point(NoButton.Location.X, NoButton.Location.Y - par-NoButton.Height);
+            }
+            int Xm = e.Location.X;
+            int Ym = e.Location.Y;
+            int X = NoButton.Location.X;
+            int Y = NoButton.Location.Y;
+            int W = NoButton.Width;
+            int H = NoButton.Height;
+            if (((Math.Abs(X - Xm) <= par) && (Math.Abs(Y - par) <= Ym) && (Ym <= Y + H)) || ((Math.Abs(Ym - Y - H) <= par) && (Math.Abs(X - par) <= Xm) && (Xm <= X + W)) || ((Math.Abs(Xm - X - W) <= par) && (Math.Abs(Y - par) <= Ym) && (Ym <= Y + H)) || ((Math.Abs(Y - Ym) <= par) && (Math.Abs(X - par) <= Xm) && (Xm <= X + W)))
+            {
+                if ((Xm <= X) && (Ym <= Y))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X + k, NoButton.Location.Y + b);
+                }
+                if ((Xm <= X) && (Ym >= Y) && (Ym <= Y + H))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X + k, NoButton.Location.Y);
+                }
+                if ((Xm <= X) && (Ym >= Y + H))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X + k, NoButton.Location.Y - b);
+                }
+                if ((Xm >= X) && (Xm <= X + W) && (Ym >= Y + H))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X, NoButton.Location.Y - b);
+                }
+                if ((Xm >= X + W) && (Ym >= Y + H))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X - k, NoButton.Location.Y - b);
+                }
+                if ((Xm >= X + W) && (Ym >= Y) && (Ym <= Y + H))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X - k, NoButton.Location.Y);
+                }
+                if ((Xm >= X + W) && (Ym <= Y))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X - k, NoButton.Location.Y + b);
+                }
+                if ((Xm >= X) && (Xm <= X + W) && (Ym <= Y))
+                {
+                    NoButton.Location = new Point(NoButton.Location.X, NoButton.Location.Y + b);
+                }
+            }
+        }
+
+        private void NoButton_Enter(object sender, EventArgs e)
+        {
+            if (k != 0)
+            {
+                YesButton.Focus();
+            }
+        }
+
+        private void NoButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            NoButton.Location = new Point(NoButton.Location.X + k, NoButton.Location.Y + b);
+        }
+    }
+}
+    
